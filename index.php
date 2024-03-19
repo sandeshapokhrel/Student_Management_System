56c794d710e52ab6091bfdf180380012e85f20ed
sandeshapokhrel <8sejal7@gmail.com>
2024-03-19 22:16:19 +0545

    add logo

--- index.php
+++ index.php
@@ -1,108 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-<!-- Font Awesome ->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-  <!-- Google Fonts -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-  <!-- Bootstrap core CSS -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Material Design Bootstrap -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
-  </head>
-  <body> 
-  <nav class="navbar navbar-expand-lg navbar-dark default-color">
-    <a class="navbar-brand" href="#"><b>SMS</b></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
-      aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Courses</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Events</a>
-        </li>
-        <!-- <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">Dropdown
-          </a>
-          <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li> -->
-      </ul>
-      <ul class="navbar-nav ml-auto nav-flex-icons">
-        
-        <li class="nav-item dropdown">
-          <?php if (isset($_SESSION['login'])) { ?>
-          <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-user mr-2"></i>Account
-          </a>
-          <div class="dropdown-menu dropdown-menu-right dropdown-default"
-            aria-labelledby="navbarDropdownMenuLink-333">
-            <a class="dropdown-item" href="/sms-project/admin/dashboard.php">Dashboard</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="logout.php">Logout</a>
-          </div>
-          <?php } else { ?>
-          <a href="login.php" class="nav-link"><i class="fa fa-user mr-2"></i>User login</a>
-          <?php } ?>
-        </li>
-      </ul>
-    </div>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Student Management System</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <nav>
+    <image src="logo.svg" alt="image"/>
   </nav>
-  <!--/.Navbar -->
-  <div>
-    <div class="container-fluid"> <!--/contaner for space and container fluid for alignment -->
-      <div class="row">
-        <div class="col-lg-6"> <!--/ larg screen 50 and small 100%-->
-        <h1 class="display-3" >School Management System </h1> 
-        <p>is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley </p>
-          <a href="" class="btn byn-lg btn-primary">CALL TO ACTION</a> <!--lg- botton for large screen-->
-        </div> 
-        <div class="col-lg-6">
-          <div class="w-50 mx-auto"> <!-- w-50 for 50% width and mx auto for horizontally center-->
-          <div class="card-body">
-          <form action="" method="post">
-          <div class="md-form">
-            <input type="text" id="form1" class="form-control">
-            <label for="form1"></label>
-
-              </div>
-            </form>
-          </div>
-          </div>
-          </div>
-      </div>
-    </div>
-  </div>
-<!-- JQuery -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-  <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+  <ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">Contact</a></li>
+    <li><a href="">Admission</a></li>
+    <li><a href="">login</a></li>
+  </ul>
 </body>
-</html> 
+</html>
