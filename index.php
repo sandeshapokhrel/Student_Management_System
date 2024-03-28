b0204d4c301750e4a7b35740a2e1da166d0756a6
sandeshapokhrel <8sejal7@gmail.com>
2024-03-28 22:26:59 +0545

    update nav

--- index.php
+++ index.php
@@ -3,18 +3,20 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
   <title>Student Management System</title>
-  <link rel="stylesheet" href="style.css">
+  
 </head>
 <body>
   <nav>
-    <image src="logo.svg" alt="image"/>
-  </nav>
+    <image src="logo.svg" alt="image" class="logo" />
+  
   <ul>
     <li><a href="">Home</a></li>
     <li><a href="">Contact</a></li>
     <li><a href="">Admission</a></li>
     <li><a href="">login</a></li>
   </ul>
+  </nav>
 </body>
 </html>
